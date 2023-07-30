1850ec3bf3a3183baa207cebcc9572c667af6e15
rav-nidhi <140927437+rav-nidhi@users.noreply.github.com>
2023-07-29 20:31:49 -0500

    Update audiocd.h

--- audiocd.h
+++ audiocd.h
@@ -1,12 +1,4 @@
-/*
- * Name: Nidhi Ravala
- * EUID: nsr0065
- * Date: 18 April 2023
- * Course: CSCE 1040
- * Assignment: Homework 4
- */
- 
-//Program Description: Class defintion for audio cd
+//class defintion for audio cd
 
 #ifndef CD_H
 #define CD_H
