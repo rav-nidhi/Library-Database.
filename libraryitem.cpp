0f1840f00701a47d2566483fcb990b349c32cf59
rav-nidhi <140927437+rav-nidhi@users.noreply.github.com>
2023-07-29 20:32:46 -0500

    Update libraryitem.cpp

--- libraryitem.cpp
+++ libraryitem.cpp
@@ -1,12 +1,4 @@
-/*
- * Name: Nidhi Ravala
- * EUID: nsr0065
- * Date: 18 April 2023
- * Course: CSCE 1040
- * Assignment: Homework 4
- */
- 
-//Program Description: Function definitions of library item
+//function definitions of library item
 
 #include <iostream>
 using namespace std;
