33639545ed5313797e21c0f86c757a17ff844b7e
rav-nidhi <140927437+rav-nidhi@users.noreply.github.com>
2023-07-29 20:32:38 -0500

    Update dvd.h

--- dvd.h
+++ dvd.h
@@ -1,12 +1,4 @@
-/*
- * Name: Nidhi Ravala
- * EUID: nsr0065
- * Date: 18 April 2023
- * Course: CSCE 1040
- * Assignment: Homework 4
- */
- 
-//Program Description: Class defintion for dvd
+//class defintion for dvd
 
 #ifndef DVD_H
 #define DVD_H
