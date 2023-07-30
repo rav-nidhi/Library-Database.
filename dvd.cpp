52f9ca04203d79cb66db6b236990ff50274f24e8
rav-nidhi <140927437+rav-nidhi@users.noreply.github.com>
2023-07-29 20:32:27 -0500

    Update dvd.cpp

--- dvd.cpp
+++ dvd.cpp
@@ -1,12 +1,4 @@
-/*
- * Name: Nidhi Ravala
- * EUID: nsr0065
- * Date: 18 April 2023
- * Course: CSCE 1040
- * Assignment: Homework 4
- */
- 
-//Program Description: Function definitions of dvd
+//function definitions of dvd
 
 #include <iostream>
 using namespace std;
