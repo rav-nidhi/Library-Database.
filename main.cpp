07fa357882a6490e12c97173d8478471f0e35d38
rav-nidhi <140927437+rav-nidhi@users.noreply.github.com>
2023-07-29 20:31:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-/*
- * Name: Nidhi Ravala
- * EUID: nsr0065
- * Date: 18 April 2023
- * Course: CSCE 1040
- * Assignment: Homework 4
- */
- 
-//Program Description: Class defintion for patron
+//main file for menu options
 
 #include <iostream>
 #include <fstream>
